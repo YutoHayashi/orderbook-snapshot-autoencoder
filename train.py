6a26833af06b2c37c1ac315a6c8bd7e28e802d1f
YutoHayashi <yutohayashi@hotmail.com>
2025-10-18 13:32:15 +0900

    パスミス修正

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.abspath(__file__), 'src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
 import argparse
 import json
